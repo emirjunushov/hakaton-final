90a16218f5342c7fb018b2450615f68789b9aa02
Bekzat <Bekzat@Bekzat-MacBook-Pro.local>
2023-04-14 16:06:34 +0600

    qwe

--- src/components/Pages/Contacts/Contacts.jsx
+++ src/components/Pages/Contacts/Contacts.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Contacts = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        Тел: <a href="">+996 000 123 456</a>
+        Почта:{" "}
+        <a href="mailto:batirhakaton@gmail.com" target="_blanck">
+          batirhakaton@gmail.com
+        </a>
+      </div>
     </div>
   );
 };
