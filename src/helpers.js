9469c82a184350c8583424550294619c92a12d39
emirjunushov <emirjunushov2004@gmail.com>
2023-04-19 12:49:14 +0600

    admin

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,10 @@
 export const API = "http://34.88.174.223";
-export const ADMIN = "batirhakaton@gmail.com";
+export const ADMIN = "Batir";
 
 export const CART = {
   GET_CART: "GET_CART",
   GET_CART_LENGTH: "GET_CART_LENGTH",
 };
-
 
 export function getCountProductsInCart() {
   const cart = JSON.parse(localStorage.getItem("cart"));
@@ -16,5 +15,5 @@ export const calcSubPrice = (product) => +product.count * product.item.price;
 export const calcTotalPrice = (products) => {
   return products.reduce((acc, curr) => {
     return (acc += curr.subPrice);
-  },0);
+  }, 0);
 };
